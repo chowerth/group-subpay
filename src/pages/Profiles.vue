60ce18d6b36e683fa0f70434637d463147caf95d
gunloc0 <tony.thephasone@gmail.com>
2021-04-22 13:51:47 -0400

    removing API backend components

---
+++ src/pages/Profiles.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!-- Title bar to identify the page -->
+
+    <!-- Profiles not attached to a table, but probably a component that has editability -->
+
+    <!-- Profile row by row. This is where we could read an S3 database by calling an API possibly -->
+    
+  </q-page>
+</template>
+
+<script>
+//import GroupSubpayComponent from "../components/GroupSubpayComponent.vue";
+
+export default {
+//  components: { GroupSubpayComponent },
+//  name: "GroupSubpay"
+};
+</script>
